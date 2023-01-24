95c1b82efe01310bdc288780c4959eff0599825e
Konstantin <irongrol@gmail.com>
2023-01-24 02:40:34 -0800

    add AI navigation and movement

---
+++ Source/SimpleShooter/ShooterAIController.cpp
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ShooterAIController.h"
+#include "Kismet/GameplayStatics.h"
+
+void AShooterAIController::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AShooterAIController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+	if (LineOfSightTo(PlayerPawn)) {
+		SetFocus(PlayerPawn);
+		MoveToActor(PlayerPawn, 200);
+	}
+	else {
+		ClearFocus(EAIFocusPriority::Gameplay);
+		StopMovement();
+	}
+}
